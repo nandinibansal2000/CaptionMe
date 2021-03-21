74e5ba8d33e182148b8d23a5fc99334135f4b76d
dibyaaaaax <dibya.gautam7@gmail.com>
2021-03-21 17:18:51 +0530

    Modify utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from sklearn.model_selection import train_test_split
+import numpy as np
 
 
 def train_test_split_(img_to_captions, img_to_image_features, test_size=0.3):
@@ -29,6 +30,17 @@ def get_common_images(img_to_captions, img_to_image_features):
             img_to_cap[img] = img_to_captions[img]
 
     return img_to_image, img_to_cap
+
+
+def build_embedding_matrix(vocab_size, embedding_dim, word_to_idx, embdgs_map):
+    embedding_matrix = np.zeros((vocab_size, embedding_dim))
+
+    for word, i in word_to_idx.items():
+        embedding_vector = embdgs_map.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[i] = embedding_vector
+
+    return embedding_matrix
 
 
 # train_images, test_images, train_captions, test_captions = train_test_split_(img_to_captions, img_to_image_features)
